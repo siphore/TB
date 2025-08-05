3e2ebf0bc712bcfc5921377356a5eaff223af3c0
Michael <michael.cheneval@logixcube.ch>
2025-08-05 17:32:02 +0200

    Attempt at fixing CORS #15

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     return res.sendStatus(204);
   }
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
