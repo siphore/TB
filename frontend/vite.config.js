9e4563fc7fca0456a200b06ed1322305c1858907
Michael <michael.cheneval@logixcube.ch>
2025-08-05 17:28:21 +0200

    Attempt at fixing CORS #14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:4000",
-        target: "https://api.invoice-review.ouidoo.ch",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
