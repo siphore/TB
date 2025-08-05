c9dc002f6aa568e57935a77ffda6e9be526c1d8a
Michael <michael.cheneval@logixcube.ch>
2025-08-05 13:24:41 +0200

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { WebSocketServer } from "ws";
-// import authorizeRoutes from "./routes/authorize.js";
+import authorizeRoutes from "./routes/authorize.js";
 import jobberRoutes from "./routes/softwares/jobber.js";
 import winbizRoutes from "./routes/softwares/winbiz.js";
 import webhookRoutes, { setSockets } from "./routes/webhook.js";
@@ -12,10 +12,17 @@ dotenv.config();
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+const allowedOrigins = ["https://invoice-review.ouidoo.ch", "http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    // credentials: true, // if you're using cookies or Authorization headers
+  })
+);
 app.use(express.json());
 
-// app.use("/", authorizeRoutes);
+app.use("/", authorizeRoutes);
 app.use("/", jobberRoutes);
 app.use("/", winbizRoutes);
 app.use("/", webhookRoutes);
