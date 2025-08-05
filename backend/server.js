a833c719e5f4da07d82244e191e1978b412b466e
Michael <michael.cheneval@logixcube.ch>
2025-08-05 17:16:45 +0200

    Attempt at fixing CORS #13

--- backend/server.js
+++ backend/server.js
@@ -14,39 +14,10 @@ const PORT = 4000;
 
 const corsOptions = {
   origin: ["https://invoice-review.ouidoo.ch", "http://localhost:5173"],
-  // credentials: true,
+  credentials: true,
 };
 
-// app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://invoice-review.ouidoo.ch",
-    "http://localhost:5173",
-  ];
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -64,7 +35,7 @@ const io = new Server(server, {
   cors: {
     origin: ["https://invoice-review.ouidoo.ch", "http://localhost:5173"],
     methods: ["GET", "POST"],
-    // credentials: true,
+    credentials: true,
   },
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        // target: "http://localhost:4000",
+        target: "https://api.invoice-review.ouidoo.ch",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
